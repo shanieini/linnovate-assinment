012beaf83d1b05d00e8dcb091a965db701406f93
shanieini <shani396879@gmail.com>
2025-04-19 18:06:51 +0300

    fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
 };
 
 export default nextConfig;
